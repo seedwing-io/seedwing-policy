888c1ab4f9e55d2a242e6e6f8c08feb12631df0b
Bob McWhirter <bmcwhirt@redhat.com>
2023-01-12 14:24:00 -0500

    Fix #9.
    Spawn/join parallel rekor UUID-fetching tasks.

--- seedwing-policy-engine/src/core/sigstore/mod.rs
+++ seedwing-policy-engine/src/core/sigstore/mod.rs
@@ -4,6 +4,8 @@ use crate::package::Package;
 use crate::runtime::PackagePath;
 use crate::runtime::{Output, RuntimeError};
 use crate::value::{RationaleResult, RuntimeValue};
+use actix_rt::spawn;
+use futures_util::future::join_all;
 use sigstore::rekor::apis::configuration::Configuration;
 use sigstore::rekor::apis::{entries_api, index_api};
 use sigstore::rekor::models::SearchIndex;
@@ -47,7 +49,38 @@ impl Function for SHA256 {
                 };
                 let uuid_vec = index_api::search_index(&configuration, query).await;
                 if let Ok(uuid_vec) = uuid_vec {
-                    let mut transform: Vec<RuntimeValue> = Vec::new();
+                    let handles = uuid_vec.iter().cloned().map(|uuid| {
+                        let configuration = configuration.clone();
+                        spawn(async move {
+                            let entry =
+                                entries_api::get_log_entry_by_uuid(&configuration, uuid.as_str())
+                                    .await;
+                            if let Ok(entry) = entry {
+                                let body = base64::decode(entry.body);
+                                if let Ok(body) = body {
+                                    let body: Result<serde_json::Value, _> =
+                                        serde_json::from_slice(&*body);
+                                    if let Ok(body) = body {
+                                        let value: RuntimeValue = body.into();
+                                        //return (transform.push(value));
+                                        Some(value)
+                                    } else {
+                                        None
+                                    }
+                                } else {
+                                    None
+                                }
+                            } else {
+                                None
+                            }
+                        })
+                    });
+
+                    let joined = join_all(handles).await;
+                    let transform: Vec<RuntimeValue> =
+                        joined.into_iter().flatten().flatten().collect();
+
+                    /*
                     for uuid in uuid_vec.iter() {
                         let entry = entries_api::get_log_entry_by_uuid(&configuration, uuid).await;
                         if let Ok(entry) = entry {
@@ -62,6 +95,7 @@ impl Function for SHA256 {
                             }
                         }
                     }
+                     */
 
                     Ok(Output::Transform(Rc::new(transform.into())).into())
                 } else {
