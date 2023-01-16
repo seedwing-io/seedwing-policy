0b0dff69b98cdd310dce39594812cfffd11dae07
Ulf Lilleengen <lulf@redhat.com>
2023-01-16 13:05:22 +0100

    add benchmark with sigstore data

--- seedwing-policy-engine/benches/engine.rs
+++ seedwing-policy-engine/benches/engine.rs
@@ -2,8 +2,7 @@ use bencher::{benchmark_group, benchmark_main, Bencher};
 use seedwing_policy_engine::{lang::builder::Builder, runtime::sources::Ephemeral};
 use serde_json::json;
 
-fn eval_speed(bencher: &mut Bencher) {
-    let data = testdata1();
+fn eval_speed(bencher: &mut Bencher, data: TestData) {
     let mut builder = Builder::new();
 
     let _ = builder.build(data.src.iter());
@@ -22,35 +21,14 @@ fn eval_speed(bencher: &mut Bencher) {
     });
 }
 
-fn build_speed(bencher: &mut Bencher) {
-    let data = testdata1();
-
+fn build_speed(bencher: &mut Bencher, data: TestData) {
     bencher.iter(|| {
         let mut builder = Builder::new();
         let _ = builder.build(data.src.clone().iter());
     });
 }
 
-/*
- * TODO looks like finish multiple times will slow things down. Figure out how to 'clone' the built state.
-fn finish_speed(bencher: &mut Bencher) {
-    let data = testdata1();
-
-    let mut builder = Builder::new();
-    let _ = builder.build(data.src.iter());
-
-    let executor = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-
-    bencher.iter(|| {
-        let _ = executor.block_on(builder.finish()).unwrap();
-    });
-}*/
-
-fn end_to_end_speed(bencher: &mut Bencher) {
-    let data = testdata1();
+fn end_to_end_speed(bencher: &mut Bencher, data: TestData) {
     let executor = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
@@ -78,9 +56,26 @@ struct TestData {
     value: String,
 }
 
-fn testdata1() -> TestData {
+/*
+ * Smoke test group
+ *
+ * Basic tests with small in-memory examples.
+ */
+fn smoke_eval(bencher: &mut Bencher) {
+    eval_speed(bencher, testdata_smoke());
+}
+
+fn smoke_build(bencher: &mut Bencher) {
+    build_speed(bencher, testdata_smoke());
+}
+
+fn smoke_end_to_end(bencher: &mut Bencher) {
+    end_to_end_speed(bencher, testdata_smoke());
+}
+
+fn testdata_smoke() -> TestData {
     let src = Ephemeral::new(
-        "test1",
+        "smoke",
         r#"
         pattern named<name> = {
             name: name
@@ -96,7 +91,7 @@ fn testdata1() -> TestData {
 
     TestData {
         src,
-        path: "test1::folks".to_string(),
+        path: "smoke::folks".to_string(),
         value: json!(
             {
                 "name": "Bob",
@@ -107,5 +102,76 @@ fn testdata1() -> TestData {
     }
 }
 
-benchmark_group!(benches, build_speed, eval_speed, end_to_end_speed);
+/*
+ * Sigstore test group
+ *
+ * More complex tests using sigstore verification.
+ */
+
+fn sigstore_eval(bencher: &mut Bencher) {
+    eval_speed(bencher, testdata_sigstore());
+}
+
+fn sigstore_build(bencher: &mut Bencher) {
+    build_speed(bencher, testdata_sigstore());
+}
+
+fn sigstore_end_to_end(bencher: &mut Bencher) {
+    end_to_end_speed(bencher, testdata_sigstore());
+}
+
+fn testdata_sigstore() -> TestData {
+    let src = Ephemeral::new(
+        "foo::bar",
+        r#"
+            // Single-line comment, yay
+            pattern signed-thing = {
+                digest: sigstore::SHA256(
+                    n<1>::{
+                        apiVersion: "0.0.1",
+                        spec: {
+                            signature: {
+                                publicKey: {
+                                    content: base64::Base64(
+                                        x509::PEM( n<1>::{
+                                            version: 2,
+                                            extensions: n<1>::{
+                                                subjectAlternativeName: n<1>::{
+                                                    rfc822: "bob@mcwhirter.org",
+                                                }
+                                            }
+                                        } )
+                                    )
+                                }
+                            }
+                        }
+                    }
+                )
+            }
+        "#,
+    );
+
+    let value = json!(
+        {
+            "digest": "5dd1e2b50b89874fd086da4b61176167ae9e4b434945325326690c8f604d0408"
+        }
+    )
+    .to_string();
+
+    TestData {
+        src,
+        path: "foo::bar::signed-thing".to_string(),
+        value,
+    }
+}
+
+benchmark_group!(
+    benches,
+    smoke_build,
+    smoke_eval,
+    smoke_end_to_end,
+    sigstore_build,
+    sigstore_eval,
+    sigstore_end_to_end
+);
 benchmark_main!(benches);
