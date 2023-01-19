9906c89ce42a943cad43956d9a52cf43475a8961
Jens Reimann <jreimann@redhat.com>
2023-01-19 18:05:39 +0100

    feat: add a method to base64 encode octets
    This comes in handy when testing for the octet type:
    
    ```
    pattern foo = {
      raw: base64::Base64Encode("Zm9vCg==")
    }
    ```
    
    This could test if the field "raw" has the value
    `RuntimeValue::Octet("foo".as_bytes().into())`

--- seedwing-policy-engine/src/core/base64/mod.rs
+++ seedwing-policy-engine/src/core/base64/mod.rs
@@ -16,17 +16,19 @@ use std::sync::Arc;
 pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["base64"]));
     pkg.register_function("Base64".into(), Base64);
+    pkg.register_function("Base64Encode".into(), Base64Encode);
     pkg
 }
 
-const DOCUMENTATION: &str = include_str!("Base64.adoc");
+const DOCUMENTATION_BASE64: &str = include_str!("Base64.adoc");
+const DOCUMENTATION_BASE64_ENCODE: &str = include_str!("Base64Encode.adoc");
 
 #[derive(Debug)]
 pub struct Base64;
 
 impl Function for Base64 {
     fn documentation(&self) -> Option<String> {
-        Some(DOCUMENTATION.into())
+        Some(DOCUMENTATION_BASE64.into())
     }
 
     fn call<'v>(
@@ -46,6 +48,33 @@ impl Function for Base64 {
                     //Err(FunctionError::Other("unable to decode base64".into()))
                     Ok(Output::None.into())
                 }
+            } else {
+                Ok(Output::None.into())
+            }
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Base64Encode;
+
+impl Function for Base64Encode {
+    fn documentation(&self) -> Option<String> {
+        Some(DOCUMENTATION_BASE64_ENCODE.into())
+    }
+
+    fn call<'v>(
+        &'v self,
+        input: Rc<RuntimeValue>,
+        bindings: &'v Bindings,
+        world: &'v World,
+    ) -> Pin<Box<dyn Future<Output = Result<FunctionEvaluationResult, RuntimeError>> + 'v>> {
+        Box::pin(async move {
+            let input = (*input).borrow();
+            if let Some(inner) = input.try_get_octets() {
+                let result = base64::encode(inner);
+
+                Ok(Output::Transform(Rc::new(result.into())).into())
             } else {
                 Ok(Output::None.into())
             }
