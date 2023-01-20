6366a12dc3fca8e8e2d90a84ebcf1291b89ba175
Bob McWhirter <bmcwhirt@redhat.com>
2023-01-20 11:11:03 -0500

    Fix #29 -- Add a regexp function.

--- seedwing-policy-engine/src/core/string/mod.rs
+++ seedwing-policy-engine/src/core/string/mod.rs
@@ -1,6 +1,8 @@
 mod length;
+mod regexp;
 
 use crate::core::string::length::Length;
+use crate::core::string::regexp::Regexp;
 use crate::core::{Function, FunctionEvaluationResult};
 use crate::lang::lir::Bindings;
 use crate::package::Package;
@@ -13,5 +15,6 @@ use std::rc::Rc;
 pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["string"]));
     pkg.register_function("Length".into(), Length);
+    pkg.register_function("Regexp".into(), Regexp);
     pkg
 }
---
+++ seedwing-policy-engine/src/core/string/regexp.rs
@@ -0,0 +1,117 @@
+use crate::core::{Function, FunctionEvaluationResult};
+use crate::lang::lir::{Bindings, InnerType, ValueType};
+use crate::runtime::{Output, RuntimeError, World};
+use crate::value::RuntimeValue;
+use regex::Regex;
+use std::future::Future;
+use std::pin::Pin;
+use std::rc::Rc;
+
+const DOCUMENTATION: &str = include_str!("Regexp.adoc");
+const REGEXP: &str = "regexp";
+
+#[derive(Debug)]
+pub struct Regexp;
+
+impl Function for Regexp {
+    fn documentation(&self) -> Option<String> {
+        Some(DOCUMENTATION.into())
+    }
+
+    fn parameters(&self) -> Vec<String> {
+        vec![REGEXP.into()]
+    }
+
+    fn call<'v>(
+        &'v self,
+        input: Rc<RuntimeValue>,
+        bindings: &'v Bindings,
+        world: &'v World,
+    ) -> Pin<Box<dyn Future<Output = Result<FunctionEvaluationResult, RuntimeError>> + 'v>> {
+        Box::pin(async move {
+            if let Some(regexp) = bindings.get(REGEXP) {
+                if let InnerType::Const(ValueType::String(regexp)) = regexp.inner() {
+                    if let Some(value) = input.try_get_string() {
+                        if let Ok(regexp) = Regex::new(regexp.as_str()) {
+                            if regexp.is_match(value.as_str()) {
+                                return Ok(Output::Identity.into());
+                            }
+                        }
+                    }
+                }
+            }
+            Ok(Output::None.into())
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::lang::builder::Builder;
+    use crate::runtime::sources::Ephemeral;
+    use serde_json::json;
+
+    #[actix_rt::test]
+    async fn call_matching_with_valid_param() {
+        let src = Ephemeral::new(
+            "test",
+            r#"
+            pattern re = string::Regexp<"bob.*mcwhirter">
+        "#,
+        );
+
+        let mut builder = Builder::new();
+
+        let result = builder.build(src.iter());
+
+        let runtime = builder.finish().await.unwrap();
+
+        let result = runtime
+            .evaluate("test::re", json!("bob dude mcwhirter"))
+            .await;
+        assert!(result.unwrap().satisfied())
+    }
+
+    #[actix_rt::test]
+    async fn call_nonmatching_with_valid_param() {
+        let src = Ephemeral::new(
+            "test",
+            r#"
+            pattern re = string::Regexp<"bob.*mcwhirter">
+        "#,
+        );
+
+        let mut builder = Builder::new();
+
+        let result = builder.build(src.iter());
+
+        let runtime = builder.finish().await.unwrap();
+
+        let result = runtime
+            .evaluate("test::re", json!("bob subgenius dobbs"))
+            .await;
+        assert!(!result.unwrap().satisfied())
+    }
+
+    #[actix_rt::test]
+    async fn call_nonmatching_with_invalid_param() {
+        let src = Ephemeral::new(
+            "test",
+            r#"
+            pattern re = string::Regexp<42>
+        "#,
+        );
+
+        let mut builder = Builder::new();
+
+        let result = builder.build(src.iter());
+
+        let runtime = builder.finish().await.unwrap();
+
+        let result = runtime
+            .evaluate("test::re", json!("bob subgenius dobbs"))
+            .await;
+        assert!(!result.unwrap().satisfied())
+    }
+}
