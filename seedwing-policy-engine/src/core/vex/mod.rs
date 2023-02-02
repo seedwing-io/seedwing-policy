f02a8c3345d1cfcd3503d90875fb1a18ef49efda
Ulf Lilleengen <lulf@redhat.com>
2023-02-02 09:28:24 +0100

    chore: rename policies to openvex

--- seedwing-policy-engine/src/core/vex/mod.rs
+++ seedwing-policy-engine/src/core/vex/mod.rs
@@ -3,6 +3,6 @@ use crate::runtime::PackagePath;
 
 pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["vex"]));
-    pkg.register_source("v0_0_0".into(), include_str!("v0_0_0.dog"));
+    pkg.register_source("openvex".into(), include_str!("openvex.dog"));
     pkg
 }
