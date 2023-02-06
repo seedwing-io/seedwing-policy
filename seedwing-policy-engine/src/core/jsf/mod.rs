639d725ef7a0ff4b91759656a1df4b8fbfd012f8
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-02-06 15:14:16 +0100

    Add package source names for jsf
    This commit adds names for the package sources registered for jsf.
    
    The motivation for this is that currently the when policies are loaded
    it looks like jfs is loaded twice:
    
    [INFO seedwing_policy_engine::lang::hir] loading jsf
    [INFO seedwing_policy_engine::lang::hir] loading jsf
    
    With the changes in the commit this will instead become:
    
    [INFO seedwing_policy_engine::lang::hir] loading jsf::algorithm
    [INFO seedwing_policy_engine::lang::hir] loading jsf::public-key
    
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- seedwing-policy-engine/src/core/jsf/mod.rs
+++ seedwing-policy-engine/src/core/jsf/mod.rs
@@ -3,8 +3,8 @@ use crate::runtime::PackagePath;
 
 pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["jsf"]));
-    pkg.register_source("".into(), include_str!("algorithm.dog"));
-    pkg.register_source("".into(), include_str!("public_key.dog"));
-    pkg.register_source("".into(), include_str!("signaturecore.dog"));
+    pkg.register_source("algorithm".into(), include_str!("algorithm.dog"));
+    pkg.register_source("public-key".into(), include_str!("public_key.dog"));
+    pkg.register_source("signaturecore".into(), include_str!("signaturecore.dog"));
     pkg
 }
