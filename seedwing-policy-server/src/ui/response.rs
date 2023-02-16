1d4e007766d81634606033e3f186070c46b0494b
Jim Crossley <jim@crossleys.org>
2023-02-16 18:28:41 -0500

    Use a let binding to remove the to_string() noise
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- seedwing-policy-server/src/ui/response.rs
+++ seedwing-policy-server/src/ui/response.rs
@@ -79,37 +79,48 @@ impl Response {
 }
 
 fn reason(rationale: &Rationale) -> String {
+    let tmp;
     match rationale {
-        Rationale::Anything => "anything is satisfied by anything".to_string(),
+        Rationale::Anything => "anything is satisfied by anything",
         Rationale::Nothing
         | Rationale::Const(_)
         | Rationale::Primordial(_)
-        | Rationale::Expression(_) => "".to_string(),
-        Rationale::Object(_) => if rationale.satisfied() {
-            "because all fields were satisfied"
-        } else {
-            "because not all fields were satisfied"
+        | Rationale::Expression(_) => "",
+        Rationale::Object(_) => {
+            if rationale.satisfied() {
+                "because all fields were satisfied"
+            } else {
+                "because not all fields were satisfied"
+            }
         }
-        .to_string(),
-        Rationale::List(_terms) => if rationale.satisfied() {
-            "because all members were satisfied"
-        } else {
-            "because not all members were satisfied"
+        Rationale::List(_terms) => {
+            if rationale.satisfied() {
+                "because all members were satisfied"
+            } else {
+                "because not all members were satisfied"
+            }
         }
-        .to_string(),
-        Rationale::Chain(_terms) => if rationale.satisfied() {
-            "because the chain was satisfied"
-        } else {
-            "because the chain was not satisfied"
+        Rationale::Chain(_terms) => {
+            if rationale.satisfied() {
+                "because the chain was satisfied"
+            } else {
+                "because the chain was not satisfied"
+            }
         }
-        .to_string(),
-        Rationale::NotAnObject => "not an object".to_string(),
-        Rationale::NotAList => "not a list".to_string(),
-        Rationale::MissingField(name) => format!("missing field: {name}"),
-        Rationale::InvalidArgument(name) => format!("invalid argument: {name}"),
-        Rationale::Function(_, _, _) => String::new(),
+        Rationale::NotAnObject => "not an object",
+        Rationale::NotAList => "not a list",
+        Rationale::MissingField(name) => {
+            tmp = format!("missing field: {name}");
+            &tmp
+        }
+        Rationale::InvalidArgument(name) => {
+            tmp = format!("invalid argument: {name}");
+            &tmp
+        }
+        Rationale::Function(_, _, _) => "",
         Rationale::Refinement(_primary, _refinement) => todo!(),
     }
+    .into()
 }
 
 fn support(result: &EvaluationResult) -> Vec<Response> {
