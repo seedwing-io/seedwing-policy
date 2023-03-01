ea1aca95ba8d65bcac30743eb466745ceb52e6c3
Jim Crossley <jim@crossleys.org>
2023-03-01 12:17:41 -0500

    fix: rework frontend build to mitigate extraneous trunk builds
    These were mostly caused by a missing yarn.lock file, which could
    occur if `npm i` is used, as is the case for the CI workflow.
    
    Moving on from yarn in favor of `npm ci` which guarantees repeatable
    builds by reading the lock file to recreate node_modules/
    
    We first attempted to remove filename hash suffixes from the trunk
    output. This worked but it could cause caching errors in the browser.
    
    We then reverted that and tried building the assets beneath
    OUT_DIR. This seemed to work!
    
    We also now use the npm lock file instead of package.json to determine
    when a build should re-run. The package.json isn't enough as it won't
    necessarily refer to exact versions like the -lock file.
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- server/embedded-frontend/build.rs
+++ server/embedded-frontend/build.rs
@@ -11,17 +11,32 @@ fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=../../frontend/Cargo.toml");
     println!("cargo:rerun-if-changed=../../frontend/Cargo.lock");
     println!("cargo:rerun-if-changed=../../frontend/Trunk.toml");
-    println!("cargo:rerun-if-changed=../../frontend/package.json");
-    println!("cargo:rerun-if-changed=../../frontend/yarn.lock");
+    println!("cargo:rerun-if-changed=../../frontend/package-lock.json");
     println!("cargo:rerun-if-changed=../../frontend/src");
     println!("cargo:rerun-if-changed=../../frontend/assets");
 
-    let output = Command::new("trunk")
+    let npm = Command::new("npm")
+        .args(["ci"])
+        .current_dir("../../frontend")
+        .output()
+        .expect("failed to execute frontend build");
+
+    if !npm.status.success() {
+        panic!(
+            "Failed to run 'npm':\n{}\n{}",
+            String::from_utf8_lossy(&npm.stdout),
+            String::from_utf8_lossy(&npm.stderr)
+        );
+    }
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dist = Path::new(&out_dir).join("dist");
+
+    let trunk = Command::new("trunk")
         .args([
             "build",
             "--release",
             "-d",
-            "../server/embedded-frontend/dist",
+            dist.to_str().unwrap(),
             "--public-url",
             "/",
         ])
@@ -29,19 +44,17 @@ fn main() -> std::io::Result<()> {
         .output()
         .expect("failed to execute frontend build");
 
-    if !output.status.success() {
+    if !trunk.status.success() {
         panic!(
             "Failed to run 'trunk':\n{}\n{}",
-            String::from_utf8_lossy(&output.stdout),
-            String::from_utf8_lossy(&output.stderr)
+            String::from_utf8_lossy(&trunk.stdout),
+            String::from_utf8_lossy(&trunk.stderr)
         );
     }
 
-    let mut assets = resource_dir("dist");
+    let mut assets = resource_dir(dist);
     assets
-        .with_generated_filename(
-            Path::new(&env::var("OUT_DIR").unwrap()).join("generated-console.rs"),
-        )
+        .with_generated_filename(Path::new(&out_dir).join("generated-console.rs"))
         .with_generated_fn("generate_console_assets");
 
     assets.build()?;
