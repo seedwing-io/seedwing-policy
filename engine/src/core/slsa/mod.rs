e01aedf070fa4c7970c2d2e1413109d02d7348c7
Ulf Lilleengen <lulf@redhat.com>
2023-03-13 11:46:14 +0100

    adjust provenance schema and add unit test

--- engine/src/core/slsa/mod.rs
+++ engine/src/core/slsa/mod.rs
@@ -6,3 +6,17 @@ pub fn package() -> Package {
     pkg.register_source("provenance".into(), include_str!("provenance-v1_0.dog"));
     pkg
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{assert_satisfied, runtime::testutil::test_pattern};
+
+    #[tokio::test]
+    async fn test_valid_provenance() {
+        let input = include_str!("example1.json");
+        let json: serde_json::Value = serde_json::from_str(input).unwrap();
+        let result = test_pattern(r#"slsa::provenance::provenance"#, json).await;
+
+        assert_satisfied!(result);
+    }
+}
