d3a713a563493127c71de5a8e28ad2cfa63faae1
Ulf Lilleengen <lulf@redhat.com>
2023-03-13 13:18:02 +0100

    add github-specific provenance patterns

--- engine/src/core/slsa/mod.rs
+++ engine/src/core/slsa/mod.rs
@@ -4,6 +4,7 @@ use crate::runtime::PackagePath;
 pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["slsa"]));
     pkg.register_source("provenance".into(), include_str!("provenance-v1_0.dog"));
+    pkg.register_source("github".into(), include_str!("provenance-github-v1_0.dog"));
     pkg
 }
 
@@ -16,6 +17,15 @@ mod tests {
         let input = include_str!("example1.json");
         let json: serde_json::Value = serde_json::from_str(input).unwrap();
         let result = test_pattern(r#"slsa::provenance::provenance"#, json).await;
+
+        assert_satisfied!(result);
+    }
+
+    #[tokio::test]
+    async fn test_github_provenance() {
+        let input = include_str!("example2.json");
+        let json: serde_json::Value = serde_json::from_str(input).unwrap();
+        let result = test_pattern(r#"slsa::github::provenance"#, json).await;
 
         assert_satisfied!(result);
     }
