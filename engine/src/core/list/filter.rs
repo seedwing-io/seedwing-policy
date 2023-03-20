90029700cf183d7c3cf0db60b06a73687c843fc2
Ulf Lilleengen <lulf@redhat.com>
2023-03-20 13:50:37 +0100

    feat: add list::filter
    Accepts lists as input and outputs the input values that matched the
    pattern parameter

---
+++ engine/src/core/list/filter.rs
@@ -0,0 +1,85 @@
+use crate::core::{Function, FunctionEvaluationResult};
+use crate::lang::lir::Bindings;
+use crate::runtime::rationale::Rationale;
+use crate::runtime::{EvalContext, Output, RuntimeError, World};
+use crate::value::RuntimeValue;
+
+use std::future::Future;
+use std::pin::Pin;
+
+use crate::lang::PatternMeta;
+use std::sync::Arc;
+
+const DOCUMENTATION: &str = include_str!("filter.adoc");
+const PARAMETER: &str = "parameter";
+
+#[derive(Debug)]
+pub struct Filter;
+
+impl Function for Filter {
+    fn order(&self) -> u8 {
+        128
+    }
+
+    fn metadata(&self) -> PatternMeta {
+        PatternMeta {
+            documentation: Some(DOCUMENTATION.into()),
+            ..Default::default()
+        }
+    }
+
+    fn parameters(&self) -> Vec<String> {
+        vec![PARAMETER.to_string()]
+    }
+
+    fn call<'v>(
+        &'v self,
+        input: Arc<RuntimeValue>,
+        ctx: &'v EvalContext,
+        bindings: &'v Bindings,
+        world: &'v World,
+    ) -> Pin<Box<dyn Future<Output = Result<FunctionEvaluationResult, RuntimeError>> + 'v>> {
+        Box::pin(async move {
+            if let Some(binding) = bindings.get(PARAMETER) {
+                match input.as_ref() {
+                    RuntimeValue::List(inputs) => {
+                        let mut result = Vec::new();
+                        for input in inputs.iter() {
+                            if let Some(value) = binding
+                                .evaluate(input.clone(), ctx, bindings, world)
+                                .await?
+                                .output()
+                            {
+                                result.push(input.clone());
+                            }
+                        }
+                        Ok(Output::Transform(Arc::new(RuntimeValue::List(result.clone()))).into())
+                    }
+                    _ => {
+                        let msg = "Input is not a list";
+                        Ok((Output::None, Rationale::InvalidArgument(msg.into())).into())
+                    }
+                }
+            } else {
+                let msg = "Unable to find filter function";
+                Ok((Output::None, Rationale::InvalidArgument(msg.into())).into())
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{assert_satisfied, runtime::testutil::test_pattern};
+
+    #[tokio::test]
+    async fn list_filter() {
+        let json = serde_json::json!([1, 2, "foo"]);
+        let result = test_pattern(r#"list::filter<integer>"#, json).await;
+        assert_satisfied!(result);
+        assert_eq!(
+            result.output().unwrap().as_json(),
+            serde_json::json!([1, 2])
+        );
+    }
+}
