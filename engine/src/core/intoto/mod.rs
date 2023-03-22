1d0062944fdfeb82fa29345b82b5224bce814672
Ulf Lilleengen <lulf@redhat.com>
2023-03-22 12:25:23 +0100

    docs: add some package docs for attestations

--- engine/src/core/intoto/mod.rs
+++ engine/src/core/intoto/mod.rs
@@ -5,7 +5,9 @@ use crate::runtime::PackagePath;
 mod envelope;
 
 pub fn package() -> Package {
-    let mut pkg = Package::new(PackagePath::from_parts(vec!["intoto"]));
+    let mut pkg = Package::new(PackagePath::from_parts(vec!["intoto"])).with_documentation(
+        "Patterns for working with link:https://in-toto.io/[in-toto] attestations.",
+    );
     pkg.register_source("".into(), include_str!("envelope.dog"));
     #[cfg(feature = "sigstore")]
     pkg.register_function("verify-envelope".into(), envelope::Verify);
--- engine/src/core/osv/mod.rs
+++ engine/src/core/osv/mod.rs
@@ -5,7 +5,9 @@ pub(crate) mod client;
 mod purl;
 
 pub fn package() -> Package {
-    let mut pkg = Package::new(PackagePath::from_parts(vec!["osv"]));
+    let mut pkg = Package::new(PackagePath::from_parts(vec!["osv"])).with_documentation(
+        "Patterns for working with the Open Source Vulnerability database (osv.dev).",
+    );
     pkg.register_function("scan-purl".into(), purl::ScanPurl);
     pkg
 }
