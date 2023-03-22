47bbfc072c7f8fee971ca66ca786e10ec9c1542e
Jens Reimann <jreimann@redhat.com>
2023-03-22 17:34:48 +0100

    fix: update example value to new model

--- frontend/src/pages/inspector/mod.rs
+++ frontend/src/pages/inspector/mod.rs
@@ -5,7 +5,9 @@ use seedwing_policy_engine::runtime::Response;
 use yew::prelude::*;
 
 const INITIAL_VALUE: &str = r#"{
-    "name": "inspector",
+    "name": {
+      "pattern": "inspector"
+    },
     "input": "If you paste the JSON output of an evaluation into the editor, you can in drill into details on the 'Inspect' tab.",
     "satisfied": true,
     "reason": "This is just an example"
