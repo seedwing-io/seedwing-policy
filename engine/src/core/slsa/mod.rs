35a449d037c17651d600e8d47fccf172fc56c78a
Ulf Lilleengen <lulf@redhat.com>
2023-03-28 14:53:57 +0200

    docs: add package-level docs to SLSA

--- engine/src/core/slsa/mod.rs
+++ engine/src/core/slsa/mod.rs
@@ -2,7 +2,8 @@ use crate::package::Package;
 use crate::runtime::PackagePath;
 
 pub fn package() -> Package {
-    let mut pkg = Package::new(PackagePath::from_parts(vec!["slsa"]));
+    let mut pkg = Package::new(PackagePath::from_parts(vec!["slsa"]))
+        .with_documentation("Packages related to [SLSA](https://slsa.dev) documents.");
     pkg.register_source("v1_0".into(), include_str!("provenance-v1_0.dog"));
     pkg.register_source("github".into(), include_str!("provenance-github-v1_0.dog"));
     pkg.register_source("v0_2".into(), include_str!("provenance-v0_2.dog"));
