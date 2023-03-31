801e16b6822c8db74a1e93970fd20247ea70e80b
Jens Reimann <jreimann@redhat.com>
2023-03-31 10:51:29 +0200

    test: add a test for the proxy/jdom case

---
+++ engine/tests/proxy.rs
@@ -0,0 +1,23 @@
+use seedwing_policy_engine::lang::Severity;
+use seedwing_policy_engine::runtime::Response;
+use serde_json::json;
+
+mod common;
+
+#[tokio::test]
+async fn test_jdom() {
+    let input = json!({
+      "hash": "02bd61a725e8af9b0176b43bf29816d0c748b8ab951385bd127be37489325a0a",
+      "purl": "pkg:maven/org.jdom/jdom@1.1.3?type=jar&repository_url=https%3A%2F%2Frepo.maven.apache.org%2Fmaven2",
+      "url": "https://repo.maven.apache.org/maven2/org/jdom/jdom/1.1.3/jdom-1.1.3.jar"
+    });
+
+    let result = common::eval(include_str!("proxy.dog"), "not-affected", input).await;
+    assert_eq!(result.severity(), Severity::Error);
+
+    let response = Response::new(&result);
+    println!("{}", serde_json::to_string_pretty(&response).unwrap());
+
+    let response = response.collapse(Severity::Error);
+    println!("{}", serde_json::to_string_pretty(&response).unwrap());
+}
