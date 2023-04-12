f69b706529a6e8f5ca7bbb4e1a0227fab126430a
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-04-12 08:18:16 +0200

    feat: add support for SPDX 2.3
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- engine/src/core/spdx/mod.rs
+++ engine/src/core/spdx/mod.rs
@@ -8,6 +8,7 @@ pub fn package() -> Package {
     let mut pkg = Package::new(PackagePath::from_parts(vec!["spdx"]));
     pkg.register_source("license".into(), include_str!("license.dog"));
     pkg.register_source("v2_2".into(), include_str!("spdx-v2.2.dog"));
+    pkg.register_source("v2_3".into(), include_str!("spdx-v2.3.dog"));
     pkg.register_function("compatible".into(), compatible::Compatible);
     pkg.register_function("license-expr".into(), license::Expression);
     pkg
@@ -19,6 +20,15 @@ mod test {
         assert_satisfied, runtime::testutil::test_data_dir, runtime::testutil::test_pattern,
     };
     use std::fs;
+
+    #[tokio::test]
+    async fn spdx_2_3_predicate() {
+        let input =
+            fs::read_to_string(test_data_dir().join("spdx").join("v2_3_predicate.json")).unwrap();
+        let json: serde_json::Value = serde_json::from_str(&input).unwrap();
+        let result = test_pattern(r#"spdx::v2_3::predicate"#, json).await;
+        assert_satisfied!(&result);
+    }
 
     #[tokio::test]
     async fn spdx_2_2_predicate() {
